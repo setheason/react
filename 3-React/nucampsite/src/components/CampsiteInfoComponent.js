0f57f675882f25e7b2c5382e569c90655ba1ecde
SethEason <setheason11@gmail.com>
2021-06-25 18:27:10 -0500

    end of week 3, broken

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardImg, CardBody, CardTitle, CardText} from 'reactstrap'; 
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -32,7 +33,7 @@ function RenderComments({comments}) {
              <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
+                      
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
@@ -45,11 +46,21 @@ function RenderComments({comments}) {
             return (
                 <div className="container">
                     <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.campsite.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
-            );
+            )
         }
         return <div />;
     }
